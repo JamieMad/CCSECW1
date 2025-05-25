e3f1d3991f37a5ded9797055e1116629a0e23d36
u5566154 <Jamie.Madden@warwick.ac.uk>
2025-05-25 16:15:15 +0100

    nginx

--- react/my-app/src/components/login/Login.js
+++ react/my-app/src/components/login/Login.js
@@ -13,7 +13,7 @@ function Login() {
     
         const handleSubmit = (event) => {
             event.preventDefault()
-            fetch('https://api:5000/login', {
+            fetch('/api/login', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
