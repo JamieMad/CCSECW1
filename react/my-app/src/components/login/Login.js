0971b811b378914c1ff7396095fe74024257e45a
u5566154 <Jamie.Madden@warwick.ac.uk>
2025-05-25 15:34:56 +0100

    try again

--- react/my-app/src/components/login/Login.js
+++ react/my-app/src/components/login/Login.js
@@ -13,7 +13,7 @@ function Login() {
     
         const handleSubmit = (event) => {
             event.preventDefault()
-            fetch('http://localhost:5000/login', {
+            fetch('http://api:5000/login', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
